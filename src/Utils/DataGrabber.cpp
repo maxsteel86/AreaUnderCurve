07bf8ac3041b2de85394ccf0b2502dadb103a79c
maxsteel86 <maxsteel86@hotmail.com>
2017-01-24 07:41:20 -0800

    comments

--- src/Utils/DataGrabber.cpp
+++ src/Utils/DataGrabber.cpp
@@ -9,11 +9,22 @@
 
 namespace AuC_Utils
 {
+	/*
+	 * Instantiate a new DataGrabber instance to retrieve a collection of
+	 * data points from a file.
+	 * 
+	 * @param fileName: name and path to file containing data.
+	 */
 	DataGrabber::DataGrabber(string fileName)
 	{
 		this->_fileName = fileName;
 	} // end method
 	
+	/*
+	 * Retrieve a collection of data points.
+	 * 
+	 * @returns: Collection of data points as a vector.
+	 */
 	vector<DataPoint> DataGrabber::GetDataPoints()
 	{
 		ifstream inputFile(this->_fileName);
